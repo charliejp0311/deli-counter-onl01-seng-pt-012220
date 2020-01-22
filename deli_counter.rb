70e0e8fb32adc0d7ddcc68d58a16a67465ca75f4
Charles Pelton <charliejp0311@gmail.com>
2020-01-22 19:25:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,13 +4,13 @@ katz_deli = []
 def line(a)
   if a.empty?
     puts "The line is currently empty."
+  elsif a.length >= 1
+    cntr = 0
+    a.each do |name|
+      a[cntr] = "#{cntr + 1}. #{name}"
+    end
+    puts "The line is currently: #{a}"
   end
-  cntr = 0
-  a.each do |name|
-    a[cntr] = "#{cntr + 1}. #{name}"
-  end
-  a.join(" ")
-  puts "The line is currently: #{a}"
 end
 
 line(katz_deli)
