76bf3ebec9a466a400bd3efcb61569015b01e2a1
Charles Pelton <charliejp0311@gmail.com>
2020-01-22 22:28:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,6 +19,8 @@ def take_a_number(a,name)
   counter = 0
   pp a
   while counter < a.length do
+    pp a[counter]
+    pp counter
     puts "Welcome, #{a[counter]}. You are number #{counter + 1} in line."
     counter += 1
   end
